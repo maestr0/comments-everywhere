98e058824e6dce71f3a2c46d272902c9cd75b552
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-11-03 11:41:57 +0000

    aa

--- js/contentscript.js
+++ js/contentscript.js
@@ -2,7 +2,7 @@
     $("<div id='commentsEverywhereWrapper'><p class='mainHeader'>Comments Everywhere</p></div>").appendTo("body");
 
     $.get(chrome.extension.getURL("html/widget.html"), function (data) {
-        $("#commentsEverywhereWrapper").append(data);
+        $("#commentsEverywhereWrapper").append(data).find(".commentsFbTag").attr("href",document.location.href);
         $("<script type='text/javascript' src='" + chrome.extension.getURL("js/facebookCode.js") + "'></script>").appendTo("body");
     });
 
