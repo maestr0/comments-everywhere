9227c115da87556169d84b08a92ac9b237c9f1f1
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-11-03 12:08:03 +0000

    aa

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,10 +1,23 @@
 (function ($) {
-    $("<div id='commentsEverywhereWrapper'><p class='mainHeader'>Comments Everywhere</p></div>").appendTo("body");
+    window.fbWidgetVisible = false;
+    $("<div id='commentsEverywhereWidget'><p class='widgetHeader'>Comments Everywhere</p></div>").appendTo("body");
 
-    $.get(chrome.extension.getURL("html/widget.html"), function (data) {
-        $("#commentsEverywhereWrapper").append(data).find(".commentsFbTag").attr("href",document.location.href);
-        $("<script type='text/javascript' src='" + chrome.extension.getURL("js/facebookCode.js") + "'></script>").appendTo("body");
+    $("#commentsEverywhereWidget .widgetHeader").click(function () {
+        if (window.fbWidgetVisible === false) {
+            if ($("#commentsEverywhereWrapper .fb_iframe_widget").length === 0) {
+                $.get(chrome.extension.getURL("html/widget.html"), function (data) {
+                    $("<div id='commentsEverywhereWrapper'><p class='mainHeader'>Comments Everywhere</p></div>").appendTo("body");
+                    $("#commentsEverywhereWrapper").append(data).find(".commentsFbTag").attr("href", document.location.href);
+                    $("<script type='text/javascript' src='" + chrome.extension.getURL("js/facebookCode.js") + "'></script>").appendTo("body");
+                    $("#commentsEverywhereWrapper").show();
+                });
+            } else {
+                $("#commentsEverywhereWrapper").show();
+            }
+            window.fbWidgetVisible = true;
+        } else {
+            window.fbWidgetVisible = false;
+            $("#commentsEverywhereWrapper").hide();
+        }
     });
-
-
 })(jQuery);
