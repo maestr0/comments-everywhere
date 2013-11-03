e7fa54d885d65d9cad4a5a73c614d02d885eb709
Pawel Raszewski <pawel.raszewski@teamaol.com>
2013-11-03 14:26:28 +0000

    aa

--- js/facebookCode.js
+++ js/facebookCode.js
@@ -27,7 +27,7 @@ window.fbAsyncInit = function () {
     facebookJS.id = 'facebook-jssdk';
 
     // Set the new script's source to the source of the Facebook JS SDK
-    facebookJS.src = 'http://connect.facebook.net/en_US/all.js';
+    facebookJS.src = location.protocol + '//connect.facebook.net/en_US/all.js';
 
     // Insert the Facebook JS SDK into the DOM
     firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
